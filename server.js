194339d509771b9d9fea45eb4b601caf95b50d04
ngolam111019 <ngothanhlamit@gmail.com>
2025-11-01 11:13:58 +0700

    20251101

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Redirect all unknown routes to index.html
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => console.log(`✅ Server is running on port ${port}`));
