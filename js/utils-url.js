ed4678e374edd68d1c9e771fb223e4328241c934
ngolam111019 <ngothanhlamit@gmail.com>
2025-12-01 16:39:35 +0700

    fix url api

--- js/utils-url.js
+++ js/utils-url.js
@@ -1,7 +1,7 @@
 
 function getUrl() {
-  return "http://127.0.0.1:3000";
-  //return "https://tool-ai-api-4fdc58954ac0.herokuapp.com";
+  //return "http://127.0.0.1:3000";
+  return "https://tool-ai-api-4fdc58954ac0.herokuapp.com";
 }
 
 function getDeviceId() {
